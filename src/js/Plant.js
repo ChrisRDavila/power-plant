8e2032e46e88463dce5fc2370b861d6f74403e6c
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-28 09:33:19 -0800

    Pass test for changePlantState
    Co-authored-by: Brianca Knight <BriancaKnight@users.noreply.github.com>
    Co-authored-by: Christopher Ross Davila <ChrisRDavila@users.noreply.github.com>

--- src/js/Plant.js
+++ src/js/Plant.js
@@ -1,13 +1,23 @@
-export const hydrate = (plant) => {
+export const changePlantState = (plant, property) => {
   return {
     ...plant,
-    water: (plant.water || 0) + 1
+    [property]: (plant[property] || 0) + 1
   }
-};
+}
 
-export const feed = (plant) => {
-  return {
-    ...plant,
-    soil: (plant.soil || 0) + 1
-  }
-};
+
+
+
+// export const hydrate = (plant) => {
+//   return {
+//     ...plant,
+//     water: (plant.water || 0) + 1
+//   }
+// };
+
+// export const feed = (plant) => {
+//   return {
+//     ...plant,
+//     soil: (plant.soil || 0) + 1
+//   }
+// };
