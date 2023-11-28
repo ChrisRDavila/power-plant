fa92a22e658ea00d21b4ad0b0a5695c9b47a3f33
mejia-dev <aaron.mejia8484@gmail.com>
2023-11-28 14:08:42 -0800

    Add growing plant image based on soil levels
    Co-authored-by: Brianca Knight <BriancaKnight@users.noreply.github.com>
    Co-authored-by: Christopher Ross Davila <ChrisRDavila@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -1,16 +1,66 @@
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-// import OtherModule from './js/otherModule.js';
+import { changeState } from "../src/js/Plant.js";
+import { storeState } from "../src/js/Plant.js";
+import { daisy } from "../src/js/Plant.js";
+import { blueFood } from "../src/js/Plant.js";
+import { hydrate } from "../src/js/Plant.js";
+import { light } from "../src/js/Plant.js";
+import { killDaisy } from "../src/js/Plant.js";
+
+const stateControl = storeState();
 
 function handleSampleForm() {
   event.preventDefault();
-  document.getElementById("outputDiv").innerText = null;
-  const pTag = document.createElement("p");
-  pTag.append(document.getElementById("text-input").value);
-  document.getElementById("outputDiv").append(pTag);
+  document.getElementById("soil-value").innerText = null;
+  document.getElementById("water-value").innerText = null;
+  document.getElementById("light-value").innerText = null;
+  const soilP = document.createElement("p");
+  pTag.append(document.getElementById("blue-food").value);
+  document.getElementById("soil-value").append(soilP);document.getElementById("soil-value").innerText = null;
+  const waterP = document.createElement("p");
+  pTag.append(document.getElementById("hydrate").value);
+  document.getElementById("water-value").append(waterP);
+  const lightP = document.createElement("p");
+  pTag.append(document.getElementById("sunlight").value);
+  document.getElementById("light-value").append(lightP);
 }
 
-window.addEventListener("load", function() {
-  document.getElementById("sample-form").addEventListener("submit", handleSampleForm);
-});
+function increasePlantImgSize(amount) {
+  const plantImg = document.getElementById("plantImg");
+  const currentPlantSize = plantImg.style.height;
+  let numericalCurrentPlantSize = currentPlantSize.substring(0, currentPlantSize.length - 2);
+  let newPlantSize = parseInt(numericalCurrentPlantSize) + parseInt(amount);
+  plantImg.style.height = `${newPlantSize}px`;
+}
+
+
+window.onload = function() {
+
+  document.getElementById('blue-food').onclick = function() {
+    const newState = stateControl(blueFood);
+    increasePlantImgSize(newState.soil);
+    document.getElementById('soil-value').innerText = `Soil: ${newState.soil}`;
+  };
+
+  document.getElementById('hydrate').onclick = function() {
+    const newState = stateControl(hydrate);
+    increasePlantImgSize(5);
+    document.getElementById('water-value').innerText = `Water: ${currentState.water}`;
+  };
+  
+  // document.getElementById('sunlight').onclick = function() {
+  //   const newState = stateControl(sunlight);
+  //   increasePlantImgSize(5);
+  //   document.getElementById('light-value').innerText = `Sunlight: ${currentState.light}`;
+  // };
+  
+  // document.getElementById('daisy').onclick = function() {
+  //   const newState = stateControl(hydrate);
+  //   increasePlantImgSize(5);
+  //   document.getElementById('water-value').innerText = `Water: ${currentState.water}`;
+  // };
+};
+
+
